dff2321fae5d405232108777106260b8ce126ccf
Bun <95525267+meleahus@users.noreply.github.com>
2023-05-31 00:00:14 +0000

    tr

--- finalcontroller_skel.py
+++ finalcontroller_skel.py
@@ -57,67 +57,56 @@ class Final (object):
     # is going, you can use the IP header information.
     # determine the source and destination IP addresses
     ip_packet = packet.find('ipv4')
-    if not ip_packet:
-        # Not an IP packet, flood
-        self.send_packet(packet_in.buffer_id, packet_in.data, of.OFPP_FLOOD, switch_id)
-        return
-
     icmp_packet = packet.find('icmp')
-    tcp_packet = packet.find('tcp')
-    udp_packet = packet.find('udp')
 
-    # Assign host IPs to port numbers for each switch
-    ip_to_port = {
-        '10.1.1.10': 1 if switch_id == 1 else 1 if switch_id == 5 else None,
-        '10.1.2.20': 2 if switch_id == 1 else 1 if switch_id == 5 else None,
-        '10.1.3.30': 1 if switch_id == 2 else 2 if switch_id == 5 else None,
-        '10.1.4.40': 2 if switch_id == 2 else 2 if switch_id == 5 else None,
-        '10.2.5.50': 1 if switch_id == 3 else 3 if switch_id == 5 else None,
-        '10.2.6.60': 2 if switch_id == 3 else 3 if switch_id == 5 else None,
-        '10.2.7.70': 1 if switch_id == 4 else 4 if switch_id == 5 else None,
-        '10.2.8.80': 2 if switch_id == 4 else 4 if switch_id == 5 else None,
-        '10.3.9.90': 5 if switch_id == 5 else None,
-        '108.24.31.112': 1 if switch_id == 6 else 6 if switch_id == 5 else None,
-        '106.44.82.103': 2 if switch_id == 6 else 6 if switch_id == 5 else None,
-    }
-
+    # Set up flow table rules
     msg = of.ofp_flow_mod()
-    msg.match = of.ofp_match.from_packet(packet)
+    msg.match = of.ofp_match.from_packet(packet, port_on_switch)
 
-    # When the IP address of the destination host is known
-    if str(ip_packet.dstip) in ip_to_port and ip_to_port[str(ip_packet.dstip)] is not None:
-        # Rules for Untrusted Host (106.44.82.103)
-        if str(ip_packet.srcip) == '106.44.82.103':
-            # Untrusted Host cannot send ICMP traffic to Host 10 to 80, or the Server.
-            if icmp_packet or str(ip_packet.dstip) == '10.3.9.90':
+    # IP addresses for the hosts
+    hosts_A = ['10.1.1.10', '10.1.2.20', '10.1.3.30', '10.1.4.40']  # Department A
+    hosts_B = ['10.2.5.50', '10.2.6.60', '10.2.7.70', '10.2.8.80']  # Department B
+    server_ip = '10.3.9.90'
+
+    # Identify the host
+    if ip_packet:
+        src_ip = str(ip_packet.srcip)
+        dst_ip = str(ip_packet.dstip)
+
+        # Rules for Untrusted Host
+        if src_ip == '106.44.82.103':
+            if icmp_packet or dst_ip == server_ip:
                 return
-        # Rules for Trusted Host (108.24.31.112)
-        if str(ip_packet.srcip) == '108.24.31.112':
-            # Trusted Host cannot send ICMP traffic to Host 50 to 80 in Department B, or the Server.
-            if icmp_packet and (str(ip_packet.dstip).startswith('10.2') or str(ip_packet.dstip) == '10.3.9.90'):
+
+        # Rules for Trusted Host
+        if src_ip == '108.24.31.112':
+            if dst_ip == server_ip or (icmp_packet and dst_ip in hosts_B):
                 return
-        # Hosts in Department A (Host 10 to 40) cannot send any ICMP traffic to the hosts in Department B (Host 50 to 80), and vice versa.
-        if icmp_packet and ((str(ip_packet.srcip).startswith('10.1') and str(ip_packet.dstip).startswith('10.2')) or (str(ip_packet.srcip).startswith('10.2') and str(ip_packet.dstip).startswith('10.1'))):
-            return
-    # When the IP address of the destination host is known
-    if str(ip_packet.dstip) in ip_to_port and ip_to_port[str(ip_packet.dstip)] is not None:
-        # Rules for Untrusted Host (106.44.82.103)
-        if str(ip_packet.srcip) == '106.44.82.103':
-            # Untrusted Host cannot send ICMP traffic to Host 10 to 80, or the Server.
-            if icmp_packet or str(ip_packet.dstip) == '10.3.9.90':
-                return
-        # Rules for Trusted Host (108.24.31.112)
-        if str(ip_packet.srcip) == '108.24.31.112':
-            # Trusted Host cannot send ICMP traffic to Host 50 to 80 in Department B, or the Server.
-            if icmp_packet and (str(ip_packet.dstip).startswith('10.2') or str(ip_packet.dstip) == '10.3.9.90'):
-                return
-        # Hosts in Department A (Host 10 to 40) cannot send any ICMP traffic to the hosts in Department B (Host 50 to 80), and vice versa.
-        if icmp_packet and ((str(ip_packet.srcip).startswith('10.1') and str(ip_packet.dstip).startswith('10.2')) or (str(ip_packet.srcip).startswith('10.2') and str(ip_packet.dstip).startswith('10.1'))):
+
+        # ICMP traffic rules between Department A and B
+        if icmp_packet and ((src_ip in hosts_A and dst_ip in hosts_B) or (src_ip in hosts_B and dst_ip in hosts_A)):
             return
 
-        msg.actions.append(of.ofp_action_output(port = ip_to_port[str(ip_packet.dstip)]))
+        # Direct IP traffic to the correct port, based on IP address
+        if dst_ip in hosts_A or dst_ip in hosts_B or dst_ip == server_ip:
+            if switch_id == 1 and dst_ip in hosts_A:   # Switch 1 connected to Department A
+                msg.actions.append(of.ofp_action_output(port=1)) 
+            elif switch_id == 2 and dst_ip in hosts_B: # Switch 2 connected to Department B
+                msg.actions.append(of.ofp_action_output(port=1))
+            elif switch_id == 3:                       # Core switch
+                if dst_ip == server_ip:                # To Server
+                    msg.actions.append(of.ofp_action_output(port=2))
+                elif dst_ip in hosts_A:                # To Department A
+                    msg.actions.append(of.ofp_action_output(port=1))
+                elif dst_ip in hosts_B:                # To Department B
+                    msg.actions.append(of.ofp_action_output(port=3))
+        else:
+            # Flood all non-IP traffic
+            msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
     else:
-        msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+        # Flood all non-IP traffic
+        msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+  
     msg.data = packet_in
     self.connection.send(msg)
 
